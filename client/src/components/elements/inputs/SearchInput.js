e0de352ba798518c4f5f7f49de4df1022ca564fe
balthazarely <balthazar.elyj@gmail.com>
2021-12-14 15:19:35 -0700

    deleted a lot

--- client/src/components/elements/inputs/SearchInput.js
+++ client/src/components/elements/inputs/SearchInput.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import SearchResultCard from "../cards/SearchResultCard";
 import { SearchCircleIcon } from "@heroicons/react/solid";
 
